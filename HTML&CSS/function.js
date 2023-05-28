6ea5544cdec9695c8da646ad574bc6449be7a3c6
João Paulo <joaopaulo.arteagle17@gmail.com>
2023-05-28 18:17:55 -0300

    criando o objeto 'VEICULO' e permitindo ao cadastro de veiculos que salve as informações no arquivo local

--- HTML&CSS/function.js
+++ HTML&CSS/function.js
@@ -25,7 +25,9 @@ var funcionario={
  funcionario.login = login;
  funcionario.senha = senha;
  
- if(funcionario.Nome!=''){
+ if(funcionario.Nome!=' '){
+ 
+
       alert('CADASTRO EFETUADO COM SUCESSO');
       localStorage.setItem('cadastro', JSON.stringify(funcionario));
      window.history.back();
@@ -41,7 +43,45 @@ var funcionario={
  
 
 /*---------------------------------------------------------------------*/
+var veiculo={
+};
+  function cadastrarVeiculo(event){ 
+    event.preventDefault(); 
+ var NomeVeiculo = document.forms["cadastroVec"]["NomeVeiculo"].value;
+ var MarcaVeiculo = document.forms["cadastroVec"]["MarcaVeiculo"].value;
+ var idade = document.forms["cadastroVec"]["IdadeVeiculo"].value; 
+ var placaVeiculo = document.forms["cadastroVec"]["placaVeiculo"].value;
+ var Quilometragem = document.forms["cadastroVec"]["Quilometragem"].value;
+ var ModeloVeiculo = document.forms["cadastroVec"]["ModeloVeiculo"].value;
 
+
+ veiculo.NomeVeiculo = NomeVeiculo;
+ veiculo.MarcaVeiculo = MarcaVeiculo;
+ veiculo.idade = idade;
+ veiculo.placaVeiculo = placaVeiculo;
+ veiculo.Quilometragem = Quilometragem;
+ veiculo.ModeloVeiculo = ModeloVeiculo;
+ 
+ 
+ if(veiculo.NomeVeiculo!=' '){
+  console.log(veiculo.NomeVeiculo);
+  console.log(veiculo.MarcaVeiculo);
+  console.log(veiculo.idade);
+  console.log(veiculo.placaVeiculo);
+  console.log(veiculo.Quilometragem);
+  console.log(veiculo.ModeloVeiculo);
+      alert('CADASTRO EFETUADO COM SUCESSO');
+      localStorage.setItem('cadastroVec', JSON.stringify(veiculo));
+     window.history.back();
+     
+
+    }else{
+      alert('PREENCHA TODOS OS CAMPOS COMO REQUISITADO');
+
+    }
+  
+   
+ }  
 /*---------------------------------------------------------------------*/
 
  function validarDados() {
